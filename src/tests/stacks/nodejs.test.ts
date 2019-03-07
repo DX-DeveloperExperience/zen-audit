72958d10adfddc4a3bb31d115d091af874ad0aee
ig1na <greenig1na@gmail.com>
2019-03-07 10:51:18 +0100

    test(stacks): Add some more tests for paths inspection

--- src/tests/stacks/nodejs.test.ts
+++ src/tests/stacks/nodejs.test.ts
@@ -1,10 +1,26 @@
 import NodeJS from '../../stacks/nodejs';
-
-test('tests with empty path', () => {
-  expect(NodeJS.prototype.existsInPaths([])).toBeFalsy();
+test('tests with empty paths list should be falsy', () => {
+  expect(new NodeJS().existsInPaths([])).toBeFalsy();
 });
 
-test('tests with filename not at end of path', () => {
+test('tests with filename not at end of path should be falsy', () => {
   const paths = ['dummy/path/to/package.json/not-at-end'];
-  expect(NodeJS.prototype.existsInPaths(paths)).toBeFalsy();
+  expect(new NodeJS().existsInPaths(paths)).toBeFalsy();
+});
+
+test('tests with filled paths list that does not contain package.json', () => {
+  const paths = [
+    'path/not/containing1',
+    'path/not/containing2',
+    'path/not/containing3',
+  ];
+  expect(new NodeJS().existsInPaths(paths)).toBeFalsy();
+});
+
+test('tests with package.json at end of path should be truthy', () => {
+  const paths = [
+    'bad/path/to/package.json/not-at-end',
+    'right/path/to/package.json',
+  ];
+  expect(new NodeJS().existsInPaths(paths)).toBeFalsy();
 });
