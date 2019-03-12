169953e8ff552e6c7005562949ed2053fafdbf4b
Antoine L <greenig1na@gmail.com>
2019-03-12 11:10:49 +0100

    feat(stacks): Can now apply nodejs recommendations
    It is now possible to apply recommendations to NodeJS stack, that is, remove circumflex accents and
    tild of semver of dependencies
    
    BREAKING CHANGE: Can now remove every tild or circumflex accents of semver of package.json and
    package-lock.json dependencies

--- src/stacks/nodejs.ts
+++ src/stacks/nodejs.ts
@@ -1,17 +1,57 @@
 import Stack from './stack';
-import { StackRegister } from '../stack-register';
+import { StackRegister } from '../tests/stacks/stack-register';
+import * as fs from 'fs';
+import FileUtils from '../file-utils';
 /**
  * NodeJS implementation of stack interface. This class checks if the given path
  * is a project using node.
  */
 @StackRegister.register
 export default class NodeJS {
-  readonly requiredFiles: string[] = ['package-lock.json', 'package.json'];
+  readonly requiredFiles: string[] = ['./package-lock.json', './package.json'];
+  files: number[] = [];
+  errors: string[] = [];
 
-  existsInPath() {
-    return true;
+  /**
+   * Returns true if the project contains the NodeJS stack.
+   */
+  exists() {
+    return FileUtils.filesExist(this.requiredFiles);
   }
-  doSomething() {
-    console.log('do 2');
+
+  /**
+   * Apply recommendations for NodeJS projects. Here, it just removes tild or circumflex
+   * for semver.
+   */
+  apply() {
+    this.requiredFiles.forEach(file => {
+      let fileToStr = fs.readFileSync(file, { encoding: 'utf8' });
+
+      fileToStr = fileToStr.replace(
+        /(\^|\~?)(([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/g,
+        this.removeTildOrCircumflex,
+      );
+
+      fs.writeFileSync(file, fileToStr, {
+        encoding: 'utf8',
+      });
+    });
+  }
+
+  /**
+   * The replacement function that returns the semver without the tild or circumflex accents
+   * @param corresponding The string that corresponds to the Regex
+   * @param p1 The first string of sub-corresponding Regex
+   * @param p2 The second string of sub-corresponding Regex
+   */
+  removeTildOrCircumflex(corresponding: string, p1: string, p2: string) {
+    return p2;
+  }
+
+  /**
+   * Returns the name of this stack. Here, it returns NodeJS
+   */
+  getName() {
+    return 'NodeJS';
   }
 }
