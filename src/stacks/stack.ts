e2a2923a16afd24e0d015fdaf41d078eece21ae8
Antoine L <greenig1na@gmail.com>
2019-03-12 11:43:49 +0100

    feat(stack): Add exists, getName and apply functions to stack interface

--- src/stacks/stack.ts
+++ src/stacks/stack.ts
@@ -1,4 +1,21 @@
+/**
+ * The interface that defines a programmatic Stack like NodeJS, tslint..
+ */
 export default interface Stack {
+  /**
+   * An array containing all the required files of the Stack.
+   */
   readonly requiredFiles: string[];
-  existsInPath(paths: string): boolean;
+  /**
+   * Returns true if the stack exists at the root of the project.
+   */
+  exists(): boolean;
+  /**
+   * Returns the name of the Stack.
+   */
+  getName(): string;
+  /**
+   * A function that applies the recommendations.
+   */
+  apply(): void;
 }
