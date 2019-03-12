82874beba9d981a77d61e6670dc953de55e5ae98
Antoine L <greenig1na@gmail.com>
2019-03-12 15:21:13 +0100

    feat(cli): Add flags to command to show found stacks and apply recommandations
    Using ./bin/run with flag -s to list stacks and -a to apply recommandations
    
    BREAKING CHANGE: Can now list stacks and apply recommandations from command line

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Command, flags } from '@oclif/command';
 import { ListStacks } from './list-stacks';
+import Stack from './stacks/stack';
 
 class ProjectFillerCli extends Command {
   static description = 'describe the command here';
@@ -12,18 +13,35 @@ class ProjectFillerCli extends Command {
     name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
     force: flags.boolean({ char: 'f' }),
+    apply: flags.boolean({
+      char: 'a',
+      description: 'Applies recommandations to every found stack',
+    }),
+    stacks: flags.boolean({
+      char: 's',
+      description: 'Show every found stack',
+    }),
   };
 
   static args = [{ name: 'file' }];
 
   async run() {
     const { args, flags } = this.parse(ProjectFillerCli);
+    let stacks: Stack[] = [];
 
-    const stacks = ListStacks.getProjectStacks();
-    stacks.forEach(stack => {
-      this.log('Stack found : ' + stack.getName());
-      stack.apply();
-    });
+    this.log('Searching for stacks...');
+    stacks = ListStacks.getProjectStacks();
+
+    if (flags.stacks) {
+      stacks.forEach(stack => {
+        this.log('Stack found: ' + stack.getName());
+      });
+    }
+    if (flags.apply) {
+      stacks.forEach(stack => {
+        stack.apply();
+      });
+    }
   }
 }
 
