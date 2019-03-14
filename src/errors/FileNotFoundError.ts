1758a0541aede0cb30ff2a50f9617f9377467636
Antoine L <greenig1na@gmail.com>
2019-03-14 17:31:17 +0100

    refactor(Errors): Rename parameter filename into filepath

--- src/errors/FileNotFoundError.ts
+++ src/errors/FileNotFoundError.ts
@@ -1,6 +1,6 @@
 export class FileNotFoundError extends Error {
-  constructor(filename?: string) {
-    super('File not found: ' + filename);
+  constructor(filepath?: string) {
+    super('File not found: ' + filepath);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
