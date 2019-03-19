8faec3d727f7399d2368651426d30f5f1cc28d8e
Antoine L <greenig1na@gmail.com>
2019-03-19 10:36:42 +0100

    refactor(Rules): Add parsedFile and packageJSON as class members
    ADd declaration of packageJSON path string, and parsedFile json object,
    as class members, and initialise them in constructor

--- src/rules/linter.ts
+++ src/rules/linter.ts
@@ -1,3 +1,5 @@
+import { FileNotReadableError } from './../errors/FileNotReadableError';
+import { FileNotFoundError } from './../errors/FileNotFoundError';
 import FileUtils from '../file-utils';
 import { RuleRegister } from './rule-register';
 import * as fs from 'fs';
@@ -5,10 +7,30 @@ import * as fs from 'fs';
 @RuleRegister.register
 export default class Linter {
   readonly requiredFiles: string[] = ['tslint.json'];
-  rootPath: string;
+  readonly rootPath: string;
+  private packageJSON: string;
+  private parsedFile: any;
 
   constructor(rootPath: string) {
-    this.rootPath = rootPath;
+    if (rootPath === undefined) {
+      this.rootPath = rootPath;
+    } else {
+      this.rootPath = rootPath;
+    }
+
+    this.packageJSON = this.rootPath + 'package.json';
+
+    try {
+      this.parsedFile = JSON.parse(
+        fs.readFileSync(this.packageJSON, { encoding: 'utf8' }),
+      );
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        throw new FileNotFoundError(this.packageJSON);
+      } else if (err.code === 'EACCESS') {
+        throw new FileNotReadableError(this.packageJSON);
+      }
+    }
   }
 
   exists() {
@@ -22,20 +44,14 @@ export default class Linter {
     return 'Linter';
   }
 
-  apply() {}
+  apply() {
+    // TODO
+  }
 
   isInDevDep(): boolean {
-    try {
-      const parsed = JSON.parse(
-        fs.readFileSync(this.rootPath + 'package.json', {
-          encoding: 'utf8',
-        }),
-      );
-
-      return parsed.devDependencies.tslint || parsed.devDependencies.eslint;
-    } catch (err) {
-      //console.log(err);
-      return false;
-    }
+    return (
+      this.parsedFile.devDependencies.tslint !== null ||
+      this.parsedFile.devDependencies.eslint !== null
+    );
   }
 }
