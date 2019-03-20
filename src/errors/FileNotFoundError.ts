f23f96832a6b8c254723a378adce5e4f62d2fbad
Antoine L <greenig1na@gmail.com>
2019-03-20 16:03:41 +0100

    style(errors): Update FileNotFoundError.ts
    Use template string instead of concatenation

--- src/errors/FileNotFoundError.ts
+++ src/errors/FileNotFoundError.ts
@@ -1,6 +1,6 @@
 export class FileNotFoundError extends Error {
   constructor(filepath: string) {
-    super('File not found: ' + filepath);
+    super(`File not found: ${filepath}`);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
