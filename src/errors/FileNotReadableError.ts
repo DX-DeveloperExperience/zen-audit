65e9b14d45bba623c71a4880b4d558501ab99a49
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2019-03-20 15:39:31 +0100

    Update src/errors/FileNotReadableError.ts
    Replace concatenation with string template
    
    Co-Authored-By: ig1na <greenig1na@gmail.com>

--- src/errors/FileNotReadableError.ts
+++ src/errors/FileNotReadableError.ts
@@ -1,6 +1,6 @@
 export class FileNotReadableError extends Error {
   constructor(filename?: string) {
-    super('File ' + filename + ' exists, but cannot be read.');
+    super(`File ${filename} exists, but cannot be read.`);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
