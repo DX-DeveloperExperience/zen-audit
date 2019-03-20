b8c9607fb506bd2746f963d6dbab6a1328b14dc2
Antoine L <greenig1na@gmail.com>
2019-03-20 15:32:04 +0100

    test(husky rule): Add test for husky rule

---
+++ src/tests/rules/husky.test.ts
@@ -0,0 +1,46 @@
+import Husky from '../../rules/husky';
+
+const fs = require('fs');
+jest.mock('fs');
+
+test('Should return false if husky is not in devDependencies', () => {
+  const packageJSON = JSON.stringify({
+    devDependencies: {
+      dep1: 'dep1',
+      dep2: 'dep2',
+    },
+  });
+
+  fs.readFileSync.mockReturnValue(packageJSON);
+
+  expect(new Husky().isInDevDep()).toBeFalsy();
+});
+
+test('Should return true if husky is in devDependencies', () => {
+  const packageJSON = JSON.stringify({
+    devDependencies: {
+      dep1: 'dep1',
+      husky: 'dev1',
+    },
+  });
+
+  fs.readFileSync.mockReturnValue(packageJSON);
+
+  expect(new Husky().isInDevDep()).toBeTruthy();
+});
+
+test('Should return false if husky is something else than a dependency in devDependencies', () => {
+  const packageJSON = JSON.stringify({
+    devDependencies: {
+      dep1: 'husky',
+      dep2: 'dep2',
+    },
+    other: {
+      husky: 'husky',
+    },
+  });
+
+  fs.readFileSync.mockReturnValue(packageJSON);
+
+  expect(new Husky().isInDevDep()).toBeFalsy();
+});
