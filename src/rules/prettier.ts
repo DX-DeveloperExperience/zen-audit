a03f67a3d3f855e5e68a69ae1778a84419436dc9
Antoine L <greenig1na@gmail.com>
2019-03-25 11:25:03 +0100

    fix(rules): Fix bad naming in prettier class

--- src/rules/prettier.ts
+++ src/rules/prettier.ts
@@ -4,7 +4,7 @@ import { RuleRegister } from './rule-register';
 import * as fs from 'fs';
 
 @RuleRegister.register
-export default class Husky {
+export default class Prettier {
   readonly requiredFiles: string[] = ['package.json'];
   readonly rootPath: string;
   private packageJSON: string;
@@ -45,7 +45,7 @@ export default class Husky {
   }
 
   name(): string {
-    return 'Husky';
+    return 'Prettier';
   }
 
   description(): string {
