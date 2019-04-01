84f6d1dcbdb4653cd240387f06d46a148683d897
Antoine L <greenig1na@gmail.com>
2019-04-01 10:14:12 +0200

    test(Stack): Write working tests for StackRegister
    Cast the Stack and Rule class as any to be able to pass dummy classes to
    register functions

--- src/tests/stacks/stack-register.test.ts
+++ src/tests/stacks/stack-register.test.ts
@@ -1,73 +1,32 @@
 import { StackRegister } from '../../stacks/stack-register';
 
-// @StackRegister.register
-// class DummyStack {
-//   // Here DummyStack should implement Stack interface but I would like to make a mock of Stack interface so I don't
-//   // have to implement Stack methods in my tests
-// }
+class Stack1 {}
+class Stack2 {}
 
-// interface DummyStackInterface {}
-
-// class DummyStackRegisterGenerator extends RegisterGenerator<
-//   DummyStackInterface
-// > {}
-// const dummyStackRegister = new StackRegister();
-// const dummyStackRegisterGenerator = new DummyStackRegisterGenerator();
-
-// const register = dummyStackRegisterGenerator.setUpForRegister(
-//   dummyStackRegister,
-// );
-
-// @register
-// class DummyStack {}
-
-@StackRegister.register
-class DummyStack1 {
-  isInPath(path: string) {
-    return true;
-  }
-}
-
-@StackRegister.register
-class DummyStack2 {
-  isInPath(path: string) {
-    return true;
-  }
-}
-
-@StackRegister.registerRuleForStacks([DummyStack1, DummyStack2])
-class DummyRule {
-  rootPath = '';
-  name() {
-    return '';
-  }
-  description() {
-    return '';
-  }
-  requiredFiles = [];
-  shouldBeApplied() {
-    return true;
-  }
-  apply() {
-    //
-  }
-}
+class Rule1 {}
+class Rule2 {}
 
 test('getImplementations() should return an array containing two class constructors', () => {
-  expect(StackRegister.getImplementations().length).toEqual(2);
-  expect(StackRegister.getImplementations()[0].name).toEqual('DummyStack1');
-  expect(StackRegister.getImplementations()[1].name).toEqual('DummyStack2');
+  StackRegister.register(Stack1 as any);
+
+  expect(StackRegister.getImplementations().length).toBe(1);
 });
 
-test('getRulesByStack(stackName) should return an array containing the DummyRule constructor', () => {
-  const rulesArray1 = StackRegister.getRulesByStack('DummyStack1');
-  const rulesArray2 = StackRegister.getRulesByStack('DummyStack2');
+test('getRulesByStack(stackName) should return an array containing the Rule constructor', () => {
+  StackRegister.register(Stack1 as any);
+  StackRegister.register(Stack2 as any);
 
-  expect(rulesArray1).toBeDefined();
-  expect(rulesArray1.length).toEqual(1);
-  expect(rulesArray1[0].name).toEqual('DummyRule');
+  StackRegister.registerRuleForStacks([Stack1 as any, Stack2 as any])(
+    Rule1 as any,
+  );
+  StackRegister.registerRuleForStacks([Stack1 as any])(Rule2 as any);
 
-  expect(rulesArray2).toBeDefined();
-  expect(rulesArray2.length).toEqual(1);
-  expect(rulesArray2[0].name).toEqual('DummyRule');
+  const rulesArray1 = StackRegister.getRulesByStack('Stack1');
+  const rulesArray2 = StackRegister.getRulesByStack('Stack2');
+
+  expect(rulesArray1.length).toBe(2);
+  expect(rulesArray2.length).toBe(1);
+
+  expect(new rulesArray1[0]()).toBeInstanceOf(Rule1);
+  expect(new rulesArray1[1]()).toBeInstanceOf(Rule2);
 });
