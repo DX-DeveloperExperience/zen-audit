1b740eeebd5c5e91b92ea74d240984cadc465a99
Antoine L <greenig1na@gmail.com>
2019-04-11 11:53:21 +0200

    feat(FileUtils): Put back filesExistIn method

--- src/file-utils.ts
+++ src/file-utils.ts
@@ -4,6 +4,15 @@ import * as fs from 'fs';
  * A tool class to check whether one or multiple files exist or not
  */
 export default class FileUtils {
+  static filesExistIn(path: string, fileNames: string[]): boolean {
+    let found = false;
+    fileNames.forEach(fileName => {
+      found = FileUtils.findFileRecursively(path, fileName);
+    });
+
+    return found;
+  }
+
   static findFileRecursively(path: string, fileName: string): boolean {
     let found = false;
     if (fs.lstatSync(path).isFile()) {
