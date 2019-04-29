c9c36a517f4bf1b3ae89f2c1a23bc24c6a035ef3
ig1na <green_ig1na@hotmail.fr>
2019-04-29 17:12:41 +0200

    perf(list rules and stacks): Put files import in a function

--- src/rules/list-rules.ts
+++ src/rules/list-rules.ts
@@ -2,14 +2,16 @@ import { RuleRegister } from './rule-register';
 import * as fs from 'fs';
 import Rule from './rule';
 
-// import all rules files
-fs.readdirSync(`${__dirname}`)
-  .filter(path => {
-    return path.endsWith('.d.ts') || path.endsWith('.ts');
-  })
-  .forEach(path => {
-    require(`${__dirname}/${path.replace(/.d.ts|.ts/, '')}`);
-  });
+export function importRules() {
+  // import all rules files
+  fs.readdirSync(`${__dirname}`)
+    .filter(path => {
+      return path.endsWith('.d.ts') || path.endsWith('.ts');
+    })
+    .forEach(path => {
+      require(`${__dirname}/${path.replace(/.d.ts|.ts/, '')}`);
+    });
+}
 
 /**
  * Returns an array of every stack instanciated object
--- src/stacks/list-stacks.ts
+++ src/stacks/list-stacks.ts
@@ -2,14 +2,16 @@ import { StackRegister } from './stack-register';
 import * as fs from 'fs';
 import Stack from './stack';
 
-// import all stacks files
-fs.readdirSync(`${__dirname}`)
-  .filter(path => {
-    return path.endsWith('.d.ts') || path.endsWith('.ts');
-  })
-  .forEach(path => {
-    require(`${__dirname}/${path.replace(/.d.ts|.ts/, '')}`);
-  });
+export function importStacks() {
+  // import all stacks files
+  fs.readdirSync(`${__dirname}`)
+    .filter(path => {
+      return path.endsWith('.d.ts') || path.endsWith('.ts');
+    })
+    .forEach(path => {
+      require(`${__dirname}/${path.replace(/.d.ts|.ts/, '')}`);
+    });
+}
 
 export class ListStacks {
   static findStacksIn(rootPath: string): Stack[] {
