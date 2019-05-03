3d869db1f0ba018c3657bdab9f63357fa59ff4c2
ig1na <green_ig1na@hotmail.fr>
2019-05-03 16:25:59 +0200

    fix(List Stacks): Remove comment and correct getStacksIn

--- src/stacks/list-stacks/index.ts
+++ src/stacks/list-stacks/index.ts
@@ -1,18 +1,5 @@
 import { StackRegister } from '../stack-register';
-import * as fs from 'fs';
 import Stack from '../stack';
-
-// export function importStacks() {
-//   // import all stacks files
-//   const stacksDirPath = `${__dirname}/..`;
-//   fs.readdirSync(stacksDirPath)
-//     .filter(path => {
-//       return path.endsWith('.d.ts') || path.endsWith('.ts');
-//     })
-//     .forEach(path => {
-//       require(`${stacksDirPath}/${path.replace(/.d.ts|.ts/, '')}`);
-//     });
-// }
 
 export class ListStacks {
   private static stacks: Stack[] = [];
@@ -24,9 +11,11 @@ export class ListStacks {
     }
 
     if (this.stacks.length === 0 || this.path !== rootPath) {
+      this.stacks = StackRegister.getImplementations()
+        .map(stack => new stack(rootPath))
+        .filter(stack => stack.isInPath(rootPath));
     }
-    return StackRegister.getImplementations()
-      .map(stack => new stack(rootPath))
-      .filter(stack => stack.isInPath(rootPath));
+
+    return this.stacks;
   }
 }
