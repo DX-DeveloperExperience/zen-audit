52f79333e2cdce34ae8e9fd0e3ef6c212f4ea922
ig1na <green_ig1na@hotmail.fr>
2019-05-06 17:27:19 +0200

    test(GitIgnore): Add test for gitignore

--- src/rules/gitignore/gitignore.test.ts
+++ src/rules/gitignore/gitignore.test.ts
@@ -1,8 +1,10 @@
 import { GitIgnore } from '../../rules/gitignore';
 
 const fs = require('fs');
+const request = require('sync-request');
 
 jest.mock('fs');
+jest.mock('sync-request');
 
 test('shouldBeApplied should return true if .gitignore file does not exist', () => {
   fs.readFileSync.mockImplementation(() => {
@@ -14,6 +16,25 @@ test('shouldBeApplied should return true if .gitignore file does not exist', () 
 
 test('shouldBeApplied should return true if .gitignore file exists but is empty', () => {
   fs.readFileSync.mockReturnValue('');
+
+  expect(new GitIgnore().shouldBeApplied()).toBeTruthy();
+});
+
+test('shouldBeApplied should return true if .gitignore file does not contain every possible rule for a stack', () => {
+  const gitignore: string =
+    '# comment \
+    # another comment \
+    a/directory/to/ignore \
+    another/directory/to/ignore';
+
+  fs.readFileSync.mockReturnValue(gitignore);
+
+  request.getBody.toString.mockImplementation(() => {
+    return '# comment \
+            a/directory/to/ignore \
+            another/directory/to/ignore \
+            a/third/directory/to/ignore';
+  });
 
   expect(new GitIgnore().shouldBeApplied()).toBeTruthy();
 });
