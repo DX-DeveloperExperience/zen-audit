a95751a7573a7f4abb8ddeed4ae9d218ea35e62e
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-05-29 18:32:09 +0200

    fix: avoid calling getStacksIn multiple time

--- src/stacks/list-stacks/index.ts
+++ src/stacks/list-stacks/index.ts
@@ -2,24 +2,32 @@ import { StackRegister, Constructor } from '../stack-register';
 import Stack from '../stack';
 
 export class ListStacks {
+  static stacks: Stack[];
   static getStacksIn(rootPath: string) {
+    if (ListStacks.stacks) {
+      return Promise.resolve(ListStacks.stacks);
+    }
     const stackConstructors: Array<
       Constructor<Stack>
     > = StackRegister.getConstructors();
 
     const stacks = stackConstructors.map(
-      stackConstructor => new stackConstructor(),
+      stackConstructor => new stackConstructor(rootPath),
     );
 
     const isAvailablePromise = stacks.map(stack => stack.isAvailable());
 
     return Promise.all(isAvailablePromise).then(availableValues => {
-      return stacks.reduce((acc: Stack[], stack: Stack, i: number) => {
-        if (availableValues[i]) {
-          return [...acc, stack];
-        }
-        return acc;
-      }, []);
+      ListStacks.stacks = stacks.reduce(
+        (acc: Stack[], stack: Stack, i: number) => {
+          if (availableValues[i]) {
+            return [...acc, stack];
+          }
+          return acc;
+        },
+        [],
+      );
+      return ListStacks.stacks;
     });
   }
 }
