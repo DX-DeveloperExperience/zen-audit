d5d6f8122a6fbc6126fe5b05567b92c3b4b12d4f
ig1na <greenig1na@gmail.com>
2019-06-04 10:51:56 +0200

    feat(Prettier Rule): Implement apply
    Closes #62

--- src/rules/prettier/index.ts
+++ src/rules/prettier/index.ts
@@ -1,10 +1,9 @@
 import { TypeScript } from '../../stacks/typescript';
 import { Node } from '../../stacks/node';
 import { StackRegister } from '../../stacks/stack-register';
-import { FileNotReadableError } from '../../errors/FileNotReadableError';
 import { RuleRegister } from '../rule-register';
-import * as fs from 'fs';
-
+import * as util from 'util';
+import * as cp from 'child_process';
 /**
  * Looks for Prettier dependency in package.json, and add it if necessary.
  */
@@ -20,7 +19,17 @@ export class Prettier {
   }
 
   async apply() {
-    // TODO
+    const exec = util.promisify(cp.exec);
+
+    exec('npm i prettier -DE')
+      .then(() => {
+        return 'Succesfully installed prettier.';
+      })
+      .catch(() => {
+        throw new Error(
+          'Could not install prettier, try installing it using "npm i prettier -DE" command in your terminal.',
+        );
+      });
   }
 
   async shouldBeApplied() {
