dca337f1a0d612abc8032e6304bc695da79b68c1
ig1na <green_ig1na@hotmail.fr>
2019-06-11 14:58:20 +0200

    feat(isAvailable): Check that .git is a directory first

--- src/stacks/github/index.ts
+++ src/stacks/github/index.ts
@@ -2,6 +2,7 @@ import { StackRegister } from '../stack-register';
 import * as util from 'util';
 import * as cp from 'child_process';
 import { logger } from '../../logger/index';
+import * as fs from 'fs-extra';
 
 @StackRegister.register
 export default class GitHub {
@@ -10,12 +11,23 @@ export default class GitHub {
   async isAvailable(): Promise<boolean> {
     const exec = util.promisify(cp.exec);
 
-    return exec(`git remote -v`, { cwd: this.rootPath })
-      .then((value: { stdout: string; stderr: string }) => {
-        return value.stdout.includes('github.com');
+    return fs
+      .lstat(`${this.rootPath}.git`)
+      .then(lstat => {
+        if (!lstat.isDirectory()) {
+          return false;
+        }
+        return exec(`git remote -v`, { cwd: this.rootPath })
+          .then((value: { stdout: string }) => {
+            return value.stdout.includes('github.com');
+          })
+          .catch(err => {
+            logger.error('Could not execute "git" command, is git installed ?');
+            logger.debug(err);
+            return false;
+          });
       })
       .catch(err => {
-        logger.error('Could not execute "git" command, is git installed ?');
         logger.debug(err);
         return false;
       });
