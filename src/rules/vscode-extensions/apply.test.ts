7dc04a9f35587b0ac38c3d57e2058c5af5faff3e
ig1na <green_ig1na@hotmail.fr>
2019-06-11 15:56:06 +0200

    test(apply): Remove useless test and update apply test

--- src/rules/vscode-extensions/apply.test.ts
+++ src/rules/vscode-extensions/apply.test.ts
@@ -1,51 +1,9 @@
 import { VSCodeExtensions } from '.';
-import * as fs from 'fs-extra';
 
-const mockFS = require('mock-fs');
-
-afterAll(() => {
-  mockFS.restore();
-});
+const fs = require('fs-extra');
+jest.mock('fs-extra');
 
 const rootPath = 'root/';
-
-test('should create the .vscode/extensions.json file with recommendations even if folder and file does not exist', () => {
-  mockFS({
-    root: {
-      folder: {
-        'extensions.json': '',
-      },
-      '.vcode': {},
-    },
-  });
-
-  function vscodeIsDirectory() {
-    fs.lstatSync('root/.vscode').isDirectory();
-  }
-
-  function extensionsIsFile() {
-    fs.lstatSync('root/.vscode/extensions.json').isFile();
-  }
-
-  expect(vscodeIsDirectory).toThrow();
-  expect(extensionsIsFile).toThrow();
-
-  return new VSCodeExtensions(rootPath)
-    .apply(['extension1', 'extension2'])
-    .then(() => {
-      expect(fs.lstatSync('root/.vscode').isDirectory()).toBeTruthy();
-      expect(
-        fs.lstatSync('root/.vscode/extensions.json').isFile(),
-      ).toBeTruthy();
-      return fs
-        .readFile('root/.vscode/extensions.json', { encoding: 'utf-8' })
-        .then(result => {
-          const extensionsJSON = JSON.parse(result);
-          expect(extensionsJSON.recommendations[0]).toEqual('extension1');
-          expect(extensionsJSON.recommendations[1]).toEqual('extension2');
-        });
-    });
-});
 
 test('should not add extensions if they already exist', () => {
   const extensionsJSON = {
@@ -56,17 +14,24 @@ test('should not add extensions if they already exist', () => {
     virtual: true,
   });
 
-  return new VSCodeExtensions(rootPath)
-    .apply(['extension1', 'extension3'])
+  const vscodeExtensions = new VSCodeExtensions(rootPath);
+
+  let writtenJSON: string;
+  fs.writeJSON.mockImplementation((_A: string, resultJSON: any, _B: {}) => {
+    writtenJSON = JSON.stringify(resultJSON);
+  });
+
+  fs.ensureFile.mockImplementation(() => {
+    return Promise.resolve();
+  });
+
+  const expected = JSON.stringify({
+    recommendations: ['extension1', 'extension2', 'extension3'],
+  });
+
+  return vscodeExtensions
+    .apply(['extension1', 'extension2', 'extension3'])
     .then(() => {
-      return fs
-        .readFile('root/.vscode/extensions.json', { encoding: 'utf-8' })
-        .then(result => {
-          const extensionsJSONRead = JSON.parse(result);
-          expect(extensionsJSONRead.recommendations.length).toEqual(3);
-          expect(extensionsJSONRead.recommendations[0]).toEqual('extension1');
-          expect(extensionsJSONRead.recommendations[1]).toEqual('extension2');
-          expect(extensionsJSONRead.recommendations[2]).toEqual('extension3');
-        });
+      expect(writtenJSON).toEqual(expected);
     });
 });
