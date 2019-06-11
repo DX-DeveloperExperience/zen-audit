06d048d1deedbc96824edb57c8f3cf2478fd8aba
ig1na <green_ig1na@hotmail.fr>
2019-06-11 17:15:24 +0200

    fix(apply ): Add init promise in apply

--- src/rules/linter/index.ts
+++ src/rules/linter/index.ts
@@ -9,6 +9,7 @@ import * as fs from 'fs-extra';
 import * as cp from 'child_process';
 import * as util from 'util';
 import { hasDevDependencies } from '../rules-utils';
+import { YesNo } from '../../choice/index';
 
 @RuleRegister.register
 @StackRegister.registerRuleForStacks([TypeScript, Node])
@@ -54,56 +55,59 @@ export class Linter {
   }
 
   async apply(apply: boolean) {
-    if (apply) {
-      const exec = util.promisify(cp.exec);
-      return this.init()
-        .then(() => {
-          if (!this.linterInDevDep) {
-            const installCmd =
-              this.linterChoice === 'tslint'
-                ? 'npm i tslint typescript -DE'
-                : 'npm i eslint -DE';
+    return this.init().then(() => {
+      if (apply) {
+        const exec = util.promisify(cp.exec);
+        return this.init()
+          .then(() => {
+            if (!this.linterInDevDep) {
+              const installCmd =
+                this.linterChoice === 'tslint'
+                  ? 'npm i tslint typescript -DE'
+                  : 'npm i eslint -DE';
 
-            return exec(installCmd, { cwd: this.rootPath })
-              .then(() => {
-                logger.info(`Installed ${this.linterChoice} succesfully`);
-              })
-              .catch(() => {
-                logger.error(
-                  `Could notCould not install ${
-                    this.linterChoice
-                  }, try installing it using "${installCmd}" command.`,
-                );
-              });
-          } else {
-            logger.info(`${this.linterChoice} already installed.`);
-          }
-        })
-        .then(() => {
-          if (!this.linterhasConfigFile) {
-            const documentation: { [linter: string]: string } = {
-              tslint: 'https://palantir.github.io/tslint/usage/configuration/',
-              eslint: 'https://eslint.org/docs/user-guide/configuring',
-            };
-            this.writeLinterFile()
-              .then(() => {
-                logger.info(
-                  ` ${
-                    this.linterChoice
-                  }.json succesfully written to root folder. You may add more rules if you like, find documentation at : ${
-                    documentation[this.linterChoice]
-                  }`,
-                );
-              })
-              .catch(err => {
-                logger.error(`Error writing to ${this.linterChoice} file.`);
-                logger.debug(err);
-              });
-          } else {
-            logger.info(`${this.linterChoice}.json file already existing.`);
-          }
-        });
-    }
+              return exec(installCmd, { cwd: this.rootPath })
+                .then(() => {
+                  logger.info(`Installed ${this.linterChoice} succesfully`);
+                })
+                .catch(() => {
+                  logger.error(
+                    `Could notCould not install ${
+                      this.linterChoice
+                    }, try installing it using "${installCmd}" command.`,
+                  );
+                });
+            } else {
+              logger.info(`${this.linterChoice} already installed.`);
+            }
+          })
+          .then(() => {
+            if (!this.linterhasConfigFile) {
+              const documentation: { [linter: string]: string } = {
+                tslint:
+                  'https://palantir.github.io/tslint/usage/configuration/',
+                eslint: 'https://eslint.org/docs/user-guide/configuring',
+              };
+              this.writeLinterFile()
+                .then(() => {
+                  logger.info(
+                    ` ${
+                      this.linterChoice
+                    }.json succesfully written to root folder. You may add more rules if you like, find documentation at : ${
+                      documentation[this.linterChoice]
+                    }`,
+                  );
+                })
+                .catch(err => {
+                  logger.error(`Error writing to ${this.linterChoice} file.`);
+                  logger.debug(err);
+                });
+            } else {
+              logger.info(`${this.linterChoice}.json file already existing.`);
+            }
+          });
+      }
+    });
   }
 
   private writeLinterFile() {
@@ -143,10 +147,10 @@ export class Linter {
   }
 
   getPromptType() {
-    return 'confirm';
+    return 'list';
   }
 
   getChoices() {
-    return [{ name: 'Yes', value: true }, { name: 'No', value: false }];
+    return YesNo;
   }
 }
