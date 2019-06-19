72ed2d34d4877948bf91b56e5b293430a1c8d2b4
ig1na <green_ig1na@hotmail.fr>
2019-06-19 15:26:54 +0200

    feat(generateReport): Add folder creation and filename based on date
    Also handles errors with logger

--- src/templating/index.ts
+++ src/templating/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs-extra';
 import * as mustache from 'mustache';
 import Globals from '../utils/globals/index';
+import { logger } from '../logger/index';
 
 export function generateReport(rules: {
   rulesInfos: Array<{
@@ -10,11 +11,36 @@ export function generateReport(rules: {
   }>;
 }) {
   return fs
-    .readFile(__dirname + '/test.md', { encoding: 'utf-8' })
+    .readFile(__dirname + '/report.md', { encoding: 'utf-8' })
     .then((markdown: string) => {
       const output = mustache.render(markdown, rules);
-      return fs.writeFile(Globals.rootPath + 'report.md', output, {
-        encoding: 'utf-8',
-      });
+      const date = new Date();
+      const dateString = `${date.getFullYear()}-${date.getMonth() +
+        1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+      const reportDirPath = `${Globals.rootPath}zodit-reports`;
+      const reportFilePath = `${reportDirPath}/report-${dateString}.md`;
+      return fs
+        .ensureDir(reportDirPath)
+        .then(() => {
+          return fs
+            .writeFile(reportFilePath, output, {
+              encoding: 'utf-8',
+            })
+            .then(() => {
+              logger.info(`Generated Zodit report at: ${reportFilePath}`);
+            })
+            .catch(err => {
+              logger.error(
+                `Error trying to write report file : ${reportFilePath}`,
+              );
+              logger.debug(err.stack);
+            });
+        })
+        .catch(err => {
+          logger.error(
+            `Error trying to ensure existence of directory : ${reportDirPath}`,
+          );
+          logger.debug(err.stack);
+        });
     });
 }
