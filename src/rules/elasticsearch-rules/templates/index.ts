8eea89ea321be1f32390ba66bb90853a2787b68f
ig1na <greenig1na@gmail.com>
2019-06-19 12:53:03 +0200

    feat(getLongDescription): Add method to ElasticsearchTemplate

--- src/rules/elasticsearch-rules/templates/index.ts
+++ src/rules/elasticsearch-rules/templates/index.ts
@@ -34,6 +34,10 @@ export class ElasticsearchTemplate {
     return 'You should use templates for configuring your indices';
   }
 
+  getLongDescription() {
+    return 'Laborum exercitation incididunt nulla veniam labore esse. Pariatur adipisicing sint aliqua adipisicing culpa consequat reprehenderit excepteur eiusmod. Est irure voluptate fugiat enim minim laborum. Magna anim eiusmod consectetur voluptate. Proident ad ex laborum in adipisicing sit minim aliquip duis. Do non voluptate mollit officia consequat proident ex mollit dolore qui esse sit reprehenderit.';
+  }
+
   getPromptType() {
     return 'list';
   }
