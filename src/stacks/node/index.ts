3314cceaac85a9aea397fadcf87768646c7fba31
ig1na <green_ig1na@hotmail.fr>
2019-06-27 17:15:22 +0200

    refactor(Node Stack): Refactor class

--- src/stacks/node/index.ts
+++ src/stacks/node/index.ts
@@ -1,25 +1,28 @@
 import { StackRegister } from '../stack-register';
 import { existsPaths } from '../../utils/file-utils/index';
 import Globals from '../../utils/globals';
+import { logger } from '../../logger/index';
 
 @StackRegister.register
 export default class Node {
   private packagePath: string;
+  private packageJSON: Object = {};
+  private packageExists: boolean = false;
 
   constructor() {
     this.packagePath = `${Globals.rootPath}package.json`;
-  }
 
-  isAvailable(): Promise<boolean> {
-    return existsPaths(this.packagePath);
-  }
-
-  parsedPackage(): object {
-    if (this.isAvailable()) {
-      return require(this.packagePath);
+    try {
+      this.packageJSON = require(this.packagePath);
+      this.packageExists = true;
+    } catch (err) {
+      logger.info(`Node Stack: Error while parsing ${this.packagePath}`);
+      logger.debug(err);
     }
+  }
 
-    return {};
+  async isAvailable(): Promise<boolean> {
+    return this.packageExists;
   }
 
   name() {
