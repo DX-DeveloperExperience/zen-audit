051a472ac944b6a4906c53a254db9487a8a29f9f
ig1na <green_ig1na@hotmail.fr>
2019-06-27 17:14:55 +0200

    feat(subStacks): DIY handle subStacks
    need to find a solution to ListAvailableStacks

--- src/stacks/list-stacks/index.ts
+++ src/stacks/list-stacks/index.ts
@@ -7,20 +7,35 @@ export class ListStacks {
     if (ListStacks.stacks) {
       return Promise.resolve(ListStacks.stacks);
     }
-    const stackConstructors: Array<
-      Constructor<Stack>
-    > = StackRegister.getConstructors();
+    // const stackConstructors: Array<
+    //   Constructor<Stack>
+    // > = StackRegister.getConstructors();
 
-    const stacks = stackConstructors.map(
-      stackConstructor => new stackConstructor(rootPath),
-    );
+    // const stacks = stackConstructors.map(
+    //   stackConstructor => new stackConstructor(rootPath),
+    // );
+    const stacks = StackRegister.getStacks();
 
     const isAvailablePromise = stacks.map(stack => stack.isAvailable());
 
     return Promise.all(isAvailablePromise).then(availableValues => {
       ListStacks.stacks = stacks.reduce(
-        (acc: Stack[], stack: Stack, i: number) => {
+        (acc: Stack[], stack: Stack, i: number, stacks: Stack[]) => {
           if (availableValues[i]) {
+            const subStacks = StackRegister.getSubStacksOf(stack);
+
+            // if current stack has subStacks, instanciate them and add them to availableStacks if available
+            // if (!!subStacks && subStacks.length !== 0) {
+            //   const availableSubStacks = subStacks
+            //     .map(subStack => {
+            //       return new subStack();
+            //     })
+            //     .filter(instanciatedSubStack => {
+            //       return instanciatedSubStack.isAvailable();
+            //     });
+            //   stacks = [...stacks, ...availableSubStacks];
+            // }
+
             return [...acc, stack];
           }
           return acc;
