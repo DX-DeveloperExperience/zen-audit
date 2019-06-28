1e324501c0781c12ef138e8b6bbd2f0cfc012bd9
ig1na <greenig1na@gmail.com>
2019-06-28 14:33:10 +0200

    feat(subStacks isAvailable): Can now know if substacks are available

--- src/stacks/list-stacks/index.ts
+++ src/stacks/list-stacks/index.ts
@@ -7,36 +7,26 @@ export class ListStacks {
     if (ListStacks.stacks) {
       return Promise.resolve(ListStacks.stacks);
     }
-    // const stackConstructors: Array<
-    //   Constructor<Stack>
-    // > = StackRegister.getConstructors();
 
-    // const stacks = stackConstructors.map(
-    //   stackConstructor => new stackConstructor(rootPath),
-    // );
     const stacks = StackRegister.getStacks();
-
     const isAvailablePromise = stacks.map(stack => stack.isAvailable());
 
     return Promise.all(isAvailablePromise).then(availableValues => {
       ListStacks.stacks = stacks.reduce(
         (acc: Stack[], stack: Stack, i: number, stacks: Stack[]) => {
           if (availableValues[i]) {
+            let stacksToAdd = [stack];
             const subStacks = StackRegister.getSubStacksOf(stack);
 
             // if current stack has subStacks, instanciate them and add them to availableStacks if available
-            // if (!!subStacks && subStacks.length !== 0) {
-            //   const availableSubStacks = subStacks
-            //     .map(subStack => {
-            //       return new subStack();
-            //     })
-            //     .filter(instanciatedSubStack => {
-            //       return instanciatedSubStack.isAvailable();
-            //     });
-            //   stacks = [...stacks, ...availableSubStacks];
-            // }
+            if (!!subStacks && subStacks.length !== 0) {
+              const availableSubStacks = subStacks.filter(subStack => {
+                return subStack.isAvailable();
+              });
+              stacksToAdd = [...stacksToAdd, ...availableSubStacks];
+            }
 
-            return [...acc, stack];
+            return [...acc, ...stacksToAdd];
           }
           return acc;
         },
