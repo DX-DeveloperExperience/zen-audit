8327b4117764d730e8da2379c4b80c8b741ac26d
ig1na <greenig1na@gmail.com>
2019-07-01 10:44:03 +0200

    fix(methods): Remove unused methods

--- src/stacks/node/index.ts
+++ src/stacks/node/index.ts
@@ -1,22 +1,11 @@
 import { StackRegister } from '../stack-register';
 import { existsPaths } from '../../utils/file-utils/index';
 import Globals from '../../utils/globals';
-import { logger } from '../../logger/index';
 
 @StackRegister.register
 export default class Node {
   isAvailable(): Promise<boolean> {
     return existsPaths(Globals.packageJSONPath);
-  }
-
-  parsedPackage(): object {
-    if (this.isAvailable()) {
-      return require(Globals.packageJSONPath);
-    }
-  }
-
-  async isAvailable(): Promise<boolean> {
-    return this.packageExists;
   }
 
   name() {
