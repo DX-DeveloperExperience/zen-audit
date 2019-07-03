6672f0de9fbe19e278b1505ad260feed0693c4ab
ig1na <greenig1na@gmail.com>
2019-07-03 17:08:02 +0200

    feat(configs): Give better types to constants

--- src/rules/front-app-debug/constants.ts
+++ src/rules/front-app-debug/constants.ts
@@ -1,62 +1,85 @@
-export const configs: object = {
+import { Constructor } from './../../stacks/stack-register/index';
+import { LaunchConf } from './constants';
+import VueJS from '../../stacks/vue-js';
+import Stack from '../../stacks/stack';
+import Angular from '../../stacks/angular';
+import { React } from '../../stacks/react';
+export interface LaunchConfFile {
+  version: string;
+  configurations: LaunchConf[];
+}
+
+export interface LaunchConf {
+  type: string;
+  name?: string;
+  request?: string;
+  url?: string;
+  webRoot?: string;
+  pathMappings?: object[];
+  breakOnLoad?: boolean;
+  sourceMapPathOverrides?: object;
+  reAttach?: boolean;
+  file?: string;
+  addonPath?: string;
+}
+
+export const configs: {
+  [key: string]: { stack: Constructor<Stack>; confs: LaunchConf[] };
+} = {
   vuejs: {
-    firefox: {
-      type: 'firefox',
-      request: 'launch',
-      name: 'vuejs: firefox',
-      url: 'http://localhost:8080',
-      webRoot: '${workspaceFolder}/src',
-      pathMappings: [{ url: 'webpack:///src/', path: '${webRoot}/' }],
-    },
-    chrome: {
-      type: 'chrome',
-      request: 'launch',
-      name: 'vuejs: chrome',
-      url: 'http://localhost:8080',
-      webRoot: '${workspaceFolder}/src',
-      breakOnLoad: true,
-      sourceMapPathOverrides: {
-        'webpack:///./src/*': '${webRoot}/*',
+    stack: VueJS,
+    confs: [
+      {
+        type: 'firefox',
+        request: 'launch',
+        name: 'vuejs: firefox',
+        url: 'http://localhost:8080',
+        webRoot: '${workspaceFolder}/src',
+        pathMappings: [{ url: 'webpack:///src/', path: '${webRoot}/' }],
       },
-    },
+      {
+        type: 'chrome',
+        request: 'launch',
+        name: 'vuejs: chrome',
+        url: 'http://localhost:8080',
+        webRoot: '${workspaceFolder}/src',
+        breakOnLoad: true,
+        sourceMapPathOverrides: {
+          'webpack:///./src/*': '${webRoot}/*',
+        },
+      },
+    ],
   },
   angular: {
-    firefox: {
-      version: '0.2.0',
-      configurations: [
-        {
-          name: 'Launch index.html',
-          type: 'firefox',
-          request: 'launch',
-          reAttach: true,
-          file: '${workspaceFolder}/index.html',
-        },
-        {
-          name: 'Launch localhost',
-          type: 'firefox',
-          request: 'launch',
-          reAttach: true,
-          url: 'http://localhost/index.html',
-          webRoot: '${workspaceFolder}',
-        },
-        {
-          name: 'Attach',
-          type: 'firefox',
-          request: 'attach',
-        },
-        {
-          name: 'Launch WebExtension',
-          type: 'firefox',
-          request: 'launch',
-          reAttach: true,
-          addonPath: '${workspaceFolder}',
-        },
-      ],
-    },
-  },
-  chrome: {
-    version: '0.2.0',
-    configurations: [
+    stack: Angular,
+    confs: [
+      {
+        name: 'Launch index.html',
+        type: 'firefox',
+        request: 'launch',
+        reAttach: true,
+        file: '${workspaceFolder}/index.html',
+      },
+      {
+        name: 'Launch localhost',
+        type: 'firefox',
+        request: 'launch',
+        reAttach: true,
+        url: 'http://localhost/index.html',
+        webRoot: '${workspaceFolder}',
+      },
+      {
+        name: 'Attach',
+        type: 'firefox',
+        request: 'attach',
+      },
+      {
+        name: 'Launch WebExtension',
+        type: 'firefox',
+        request: 'launch',
+        reAttach: true,
+        addonPath: '${workspaceFolder}',
+      },
       {
         type: 'chrome',
         request: 'launch',
@@ -66,9 +89,24 @@ export const configs: object = {
       },
     ],
   },
+  react: {
+    stack: React,
+    confs: [
+      {
+        name: 'Chrome',
+        type: 'chrome',
+        request: 'launch',
+        url: 'http://localhost:3000',
+        webRoot: '${workspaceFolder}/src',
+        sourceMapPathOverrides: {
+          'webpack:///src/*': '${webRoot}/*',
+        },
+      },
+    ],
+  },
 };
 
-export const vscodeConfig: object = {
+export const vscodeConfig: LaunchConfFile = {
   version: '0.2.0',
   configurations: [],
 };
