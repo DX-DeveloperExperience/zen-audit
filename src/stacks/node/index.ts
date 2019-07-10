c5bfb3c58600f7050b168c1c582bccec490dcb08
ig1na <greenig1na@gmail.com>
2019-07-10 10:03:59 +0200

    refactor(Node): Use require instead of existsPath in isAvailable

--- src/stacks/node/index.ts
+++ src/stacks/node/index.ts
@@ -4,8 +4,17 @@ import Globals from '../../utils/globals';
 
 @StackRegister.register
 export default class Node {
-  isAvailable(): Promise<boolean> {
-    return existsPaths(Globals.packageJSONPath);
+  async isAvailable(): Promise<boolean> {
+    try {
+      require(Globals.packageJSONPath);
+      return true;
+    } catch (err) {
+      if (err.code === 'MODULE_NOT_FOUND') {
+        return false;
+      }
+      err.message = `Node Stack: Error reading file ${Globals.packageJSONPath}`;
+      throw err;
+    }
   }
 
   name() {
