cd36a23e77d737cc300c29667225219939ee8a63
igzz <greenig1na@gmail.com>
2019-07-10 16:46:48 +0200

    feat(elasticsearch): Add apply function for Integration Tests rule
    The apply method will download an external integration tests template
    and store it in the current directory, need to update the url

--- src/rules/DATA/elasticsearch/integration-tests/index.ts
+++ src/rules/DATA/elasticsearch/integration-tests/index.ts
@@ -4,10 +4,15 @@ import { StackRegister } from '../../../../stacks/stack-register';
 import Elasticsearch from '../../../../stacks/elasticsearch';
 import inquirer = require('inquirer');
 import { cli } from 'cli-ux';
+import Axios from 'axios';
+import { outputFile } from 'fs-extra';
 
 @RuleRegister.register
 @StackRegister.registerRuleForStacks([Elasticsearch])
 export class IntegrationTests {
+  private templateUrl: string =
+    'https://github.com/DX-DeveloperExperience/project-starter/releases/download/0.7.0/project-starter-cli-v0.7.0.tar.gz';
+  private templateFilePath: string = './elasticsearch-it-template.zip';
   async shouldBeApplied(): Promise<boolean> {
     cli.action.stop();
     return inquirer
@@ -26,7 +31,18 @@ export class IntegrationTests {
   }
 
   async apply(apply: boolean): Promise<void> {
-    // TODO
+    Axios.get(this.templateUrl, { responseType: 'arraybuffer' })
+      .catch(err => {
+        err.message = `ElasticSearch Integration Tests: Error retrieving template at ${
+          this.templateUrl
+        }`;
+        throw err;
+      })
+      .then((response: any) => {
+        outputFile(this.templateFilePath, response.data).catch(err => {
+          err.message = `Error writing file ${this.templateFilePath}`;
+        });
+      });
   }
 
   getName(): string {
