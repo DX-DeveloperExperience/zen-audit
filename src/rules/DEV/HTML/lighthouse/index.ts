d139ab03fd21d059423476c8d6a9b679b8d53538
igzz <greenig1na@gmail.com>
2019-07-18 14:12:21 +0200

    feat(lighthouse): Implemented rule lighthouse
    Added apply, default js script, and tests

--- src/rules/DEV/HTML/lighthouse/index.ts
+++ src/rules/DEV/HTML/lighthouse/index.ts
@@ -1,3 +1,4 @@
+import { WriteFileError } from './../../../../errors/FileErrors';
 import Choice, { YesNo } from './../../../../choice/index';
 import { Website } from './../../../../stacks/website/index';
 import { React } from './../../../../stacks/react/index';
@@ -8,12 +9,17 @@ import VueJS from '../../../../stacks/vue-js';
 import Globals from '../../../../utils/globals';
 import { IPackageJSON } from '../../../../utils/json/types/package';
 import { pathExistsInJSON } from '../../../../utils/json';
+import { installNpmDevDep } from '../../../../utils/commands';
+import { copyFile, ensureDir, readJSON, writeJSON, promises } from 'fs-extra';
+import { DirError } from '../../../../errors/DirErrors';
+import { logger } from '../../../../logger';
 
 @RuleRegister.register
-@StackRegister.registerRuleForStacks([React, Angular, VueJS, Website])
+@StackRegister.registerRuleForStacks([React, Angular, VueJS])
 export class LightHouse {
   private parsedPackage: IPackageJSON;
   private scripts: string[] | undefined;
+  private hasLighthouseScript: boolean = false;
   private hasDevdependency: boolean;
 
   constructor() {
@@ -24,19 +30,75 @@ export class LightHouse {
     ]);
     if (this.parsedPackage.scripts !== undefined) {
       this.scripts = Object.keys(this.parsedPackage.scripts);
+      this.hasLighthouseScript = this.scripts.includes('lighthouse');
     }
   }
 
   async shouldBeApplied(): Promise<boolean> {
-    if (this.scripts !== undefined) {
-      return !this.scripts.includes('lighthouse');
-    }
-
-    return true;
+    return !this.hasDevdependency || !this.hasLighthouseScript;
   }
 
   async apply(apply: boolean): Promise<void> {
-    // TODO
+    return new Promise((resolve, reject) => {
+      if (!this.hasDevdependency) {
+        resolve(installNpmDevDep('lighthouse'));
+      }
+      resolve();
+    }).then(() => {
+      logger.info('LightHouse: Succesfully installed LightHouse npm package.');
+      return this.createScript();
+    });
+  }
+
+  private async createScript(): Promise<void> {
+    const scriptsDirPath = Globals.rootPath + 'lighthouse-scripts/';
+    return new Promise((resolve, reject) => {
+      return ensureDir(scriptsDirPath)
+        .then(
+          () => {
+            return copyFile(
+              __dirname + '/script.js',
+              scriptsDirPath + 'lighthouse.js',
+            );
+          },
+          err => {
+            reject(new DirError(err, scriptsDirPath, this.constructor.name));
+          },
+        )
+        .then(
+          () => {
+            return readJSON(Globals.packageJSONPath);
+          },
+          err => {
+            reject(
+              new WriteFileError(
+                err,
+                scriptsDirPath + 'lighthouse.js',
+                this.constructor.name,
+              ),
+            );
+          },
+        )
+        .then(result => {
+          const packageJSON = result;
+          if (packageJSON.scripts === undefined) {
+            packageJSON.scripts = {};
+          }
+
+          if (packageJSON.scripts.lighthouse === undefined) {
+            packageJSON.scripts.lighthouse =
+              'node lighthouse-scripts/lighthouse.js';
+          }
+
+          return writeJSON(Globals.packageJSONPath, result, { spaces: '\t' });
+        })
+        .then(() => {
+          logger.info(
+            "LightHouse: Succesfully added LightHouse script. Run 'npm run lighthouse -- URL' command by replacing URL with the URL to your project to run it. Don't forget to install chrome before running the script",
+          );
+          resolve();
+        });
+    });
   }
 
   getName(): string {
