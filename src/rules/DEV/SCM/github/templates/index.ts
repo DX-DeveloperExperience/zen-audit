1b0c77f6459f77fd0ff671b47965dd481ca413f8
ig1na <greenig1na@gmail.com>
2019-07-22 15:16:04 +0200

    fix(github-templates): Use string constants instead of .md files
    Oclif's pack command removes .md files when compiling, we use string
    constants instead for template files

--- src/rules/DEV/SCM/github/templates/index.ts
+++ src/rules/DEV/SCM/github/templates/index.ts
@@ -8,6 +8,7 @@ import { YesNo } from '../../../../../choice/index';
 import { logger } from '../../../../../logger/index';
 import Choice from '../../../../../choice/index';
 import Globals from '../../../../../utils/globals/index';
+import { bugReport, custom, featureRequest } from './constants';
 
 @RuleRegister.register
 @StackRegister.registerRuleForStacks([GitHub])
@@ -18,15 +19,33 @@ export class GitHubTemplates {
   }
 
   shouldBeApplied(): Promise<boolean> {
-    return fs.pathExists(this.templatesPath).then(pathExists => {
-      if (!pathExists) {
-        return true;
-      }
-      return this.hasNoMarkdownFile();
+    return new Promise((resolve, reject) => {
+      this.hasNoMarkdownFile(this.templatesPath)
+        .then(
+          result => {
+            if (result === false) {
+              resolve(false);
+            }
+            return fs.pathExists(
+              `${Globals.rootPath}.github/ISSUE_TEMPLATE.md`,
+            );
+          },
+          err => {
+            reject(err);
+          },
+        )
+        .then(
+          (result: boolean) => {
+            resolve(!result);
+          },
+          err => {
+            reject(err);
+          },
+        );
     });
   }
 
-  async hasNoMarkdownFile() {
+  async hasNoMarkdownFile(folderPath: string) {
     return fs
       .readdir(this.templatesPath)
       .then(fileNames => {
@@ -37,37 +56,67 @@ export class GitHubTemplates {
         );
       })
       .catch(err => {
+        if (err.code === 'ENOTDIR' || err.code === 'ENOENT') {
+          return true;
+        }
         throw new DirError(err, this.templatesPath, this.constructor.name);
       });
   }
 
-  apply(answers: boolean): Promise<void> {
-    return new Promise((resolve, reject) => {
-      fs.ensureDir(this.templatesPath)
-        .then(
-          () => {
-            return fs.copy(`${__dirname}/template_files`, this.templatesPath);
-          },
-          err => {
-            reject(
-              new DirError(err, this.templatesPath, this.constructor.name),
-            );
-          },
-        )
-        .then(
-          () => {
-            logger.info(
-              'Succesully added issue templates file into .github/ISSUE_TEMPLATE directory. You may customize them to suit your needs.',
-            );
-            resolve();
-          },
-          err => {
-            err.message =
-              'Github Templates: Error trying to copy template files.';
-            reject(err);
-          },
-        );
-    });
+  async apply(apply: boolean): Promise<void> {
+    if (apply) {
+      return new Promise((resolve, reject) => {
+        fs.ensureDir(this.templatesPath)
+          .then(
+            () => {
+              const createBugReport = fs.writeFile(
+                `${this.templatesPath}bug_report.md`,
+                bugReport,
+                { encoding: 'utf-8' },
+              );
+              const createCustom = fs.writeFile(
+                `${this.templatesPath}custom.md`,
+                custom,
+                { encoding: 'utf-8' },
+              );
+              const createFeatReq = fs.writeFile(
+                `${this.templatesPath}feature_request.md`,
+                featureRequest,
+                { encoding: 'utf-8' },
+              );
+
+              return Promise.all([
+                createBugReport,
+                createCustom,
+                createFeatReq,
+              ]);
+            },
+            err => {
+              reject(
+                new DirError(err, this.templatesPath, this.constructor.name),
+              );
+            },
+          )
+          .then(
+            () => {
+              logger.info(
+                'Succesully added issue templates file into .github/ISSUE_TEMPLATE directory. You may customize them to suit your needs.',
+              );
+              resolve();
+            },
+            err => {
+              reject(
+                new WriteFileError(
+                  err,
+                  `${__dirname}/template_files`,
+                  this.constructor.name,
+                ),
+              );
+            },
+          );
+      });
+    }
+    return;
   }
 
   getName(): string {
