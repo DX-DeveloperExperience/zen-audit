8437d4c0aef98c3e1af7e92ef538d5f37476592f
ig1na <greenig1na@gmail.com>
2019-07-22 16:59:56 +0200

    test(github-templates): Revert changes to tests

--- src/rules/DEV/SCM/github/templates/apply.test.ts
+++ src/rules/DEV/SCM/github/templates/apply.test.ts
@@ -7,8 +7,6 @@ jest.mock('fs-extra');
 require('../../../../../logger');
 jest.mock('../../../../../logger');
 
-jest.mock('./constants');
-
 Globals.rootPath = 'github/';
 const templateDirPath = Globals.rootPath + '.github/ISSUE_TEMPLATE/';
 
@@ -20,20 +18,9 @@ test('Should ensure .github/ISSUE_TEMPLATE existence and create default template
 
   return templates.apply(true).then(() => {
     expect(fs.ensureDir).toBeCalledWith(templateDirPath);
-    expect(fs.writeFile).toBeCalledWith(
-      `${templateDirPath}bug_report.md`,
-      'bugReport',
-      { encoding: 'utf-8' },
-    );
-    expect(fs.writeFile).toBeCalledWith(
-      `${templateDirPath}feature_request.md`,
-      'featureRequest',
-      { encoding: 'utf-8' },
-    );
-    expect(fs.writeFile).toBeCalledWith(
-      `${templateDirPath}custom.md`,
-      'custom',
-      { encoding: 'utf-8' },
+    expect(fs.copy).toBeCalledWith(
+      `${__dirname}/template_files`,
+      templateDirPath,
     );
   });
 });
