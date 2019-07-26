0c2291e219935225c716a912aa1e2ffd1c11fe28
igzz <greenig1na@gmail.com>
2019-07-26 16:10:09 +0200

    test(nginx): Add test for nginx rule's apply method

---
+++ src/rules/DEV/JS-TS/FRONT/nginx/apply.test.ts
@@ -0,0 +1,25 @@
+import Globals from '../../../../../utils/globals';
+import Nginx from '.';
+
+const fs = require('fs-extra');
+
+jest.mock('fs-extra');
+
+Globals.rootPath = 'test/nginx';
+const confDirPath = Globals.rootPath + 'config';
+const confFilePath = confDirPath + '/nginx.conf';
+
+test("should copy file to project's config folder", () => {
+  fs.ensureDir = jest.fn(path => {
+    expect(path).toBe(confDirPath);
+    return Promise.resolve();
+  });
+
+  fs.copy = jest.fn((from, to) => {
+    expect(from).toBe(__dirname + 'nginx.conf');
+    expect(to).toBe(confFilePath);
+    return Promise.resolve();
+  });
+
+  new Nginx().apply(true);
+});
