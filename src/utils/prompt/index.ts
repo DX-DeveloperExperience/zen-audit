c809bbe24fa422a571c82ce8daa49f5fb5202fca
ig1na <greenig1na@gmail.com>
2019-08-01 14:35:08 +0200

    feat(prompt): Finish prompt functions
    SubRules are now prompted just after their parent, even if rules are
    after them, logger infos are now correctly shown and do not overlap
    anymore

--- src/utils/prompt/index.ts
+++ src/utils/prompt/index.ts
@@ -1,40 +1,8 @@
 import Rule from '../../rules/rule';
 import inquirer = require('inquirer');
-import { logger } from '../../logger';
 import { RuleRegister } from '../../rules/rule-register';
 import { Subject } from 'rxjs';
 const rx = require('rxjs');
-
-// export const ui = new inquirer.ui.BottomBar();
-
-/* export async function promptForRule(rule: Rule): Promise<void> {
-  if (rule.shouldBeApplied()) {
-    return inquirer
-      .prompt([
-        {
-          name: rule.getName(),
-          message: rule.getShortDescription(),
-          type: rule.getPromptType(),
-          choices: await rule.getChoices(),
-        },
-      ])
-      .then(async answer => {
-        const apply = rule.apply;
-
-        if (apply) {
-          await apply.call(rule, answer);
-        }
-      })
-      .then(() => {
-        logger.info(`Rule ${rule.getName()} applied succesfully`);
-
-        const subRules = RuleRegister.getSubRulesOf(rule);
-        if (subRules.length !== 0) {
-          return promptForRules(subRules);
-        }
-      });
-  }
-} */
 
 function addRulesToPrompts(prompts: Subject<any>, rules: Rule[]) {
   rules.forEach(async rule => {
@@ -53,53 +21,33 @@ function addRulesToPrompts(prompts: Subject<any>, rules: Rule[]) {
 export async function promptForRules(rules: Rule[]): Promise<void> {
   const prompts = new rx.Subject();
 
-  addRulesToPrompts(prompts, rules);
+  let i = 1;
+  addRulesToPrompts(prompts, [rules[0]]);
 
   inquirer.prompt(prompts).ui.process.subscribe((output: any) => {
     const rule: Rule = output.answer.rule;
     const apply = output.answer.rule.apply;
     const ruleAnswer = output.answer.input;
 
+    if (rules[i] === undefined) {
+      prompts.complete();
+    }
     if (apply) {
       apply.call(rule, ruleAnswer).then(() => {
         const subRules = RuleRegister.getSubRulesOf(rule);
 
         if (subRules.length !== 0) {
           addRulesToPrompts(prompts, subRules);
+        } else if (rules[i] !== undefined) {
+          addRulesToPrompts(prompts, [rules[i]]);
+          i++;
         }
       });
+    } else {
+      if (rules[i] !== undefined) {
+        addRulesToPrompts(prompts, [rules[i]]);
+        i++;
+      }
     }
   });
-
-  /* const promptsProm = rules.map(async rule => {
-    return {
-      name: rule.constructor.name,
-      message: rule.getShortDescription(),
-      type: rule.getPromptType(),
-      choices: await rule.getChoices(),
-      when: () => {},
-    } as inquirer.Question;
-  });
-
-  const prompts = await Promise.all(promptsProm); */
-
-  /* const answers: {} = await inquirer.prompt(prompts);
-
-  Object.entries(answers).forEach(([_, answer], i) => {
-    const apply = rules[i].apply;
-
-    if (apply) {
-      return apply.call(rules[i], answer).then(
-        async () => {
-          const subRules = RuleRegister.getSubRulesOf(rules[i]);
-          if (subRules.length !== 0) {
-            await promptForRules(subRules);
-          }
-        },
-        err => {
-          logger.error(err);
-        },
-      );
-    }
-  }); */
 }
