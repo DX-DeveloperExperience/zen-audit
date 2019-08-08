8795f836436c000536eee33f8acd2a9bef2f3930
ig1na <greenig1na@gmail.com>
2019-08-08 17:05:56 +0200

    fix(dockerfile): MAke shouldBeApplied always return true

--- src/rules/DEV/JS-TS/FRONT/dockerfile/index.ts
+++ src/rules/DEV/JS-TS/FRONT/dockerfile/index.ts
@@ -1,26 +1,21 @@
 import { WriteFileError } from './../../../../../errors/FileErrors';
 import { YesNo } from './../../../../../choice/index';
-import { StackRegister } from '../../../../../stacks/stack-register';
-import Angular from '../../../../../stacks/angular';
-import { React } from '../../../../../stacks/react';
-import VueJS from '../../../../../stacks/vue-js';
 import Globals from '../../../../../utils/globals';
-import { pathExists, copy } from 'fs-extra';
 import Choice from '../../../../../choice';
 import { RuleRegister } from '../../../../rule-register';
 import Nginx from '../nginx';
 import { myCopy } from '../../../../../utils/file-utils';
 import { logger } from '../../../../../logger';
 
-@StackRegister.registerRuleForStacks([Angular, React, VueJS])
 @RuleRegister.registerSubRuleOf(Nginx)
 export default class Dockerfile {
   private dockerFilePath = Globals.rootPath + 'Dockerfile';
   private defaultDockerFilePath = __dirname + '/Dockerfile';
 
   async shouldBeApplied(): Promise<boolean> {
-    return await !pathExists(this.dockerFilePath);
+    return true;
   }
+
   async apply(apply: boolean): Promise<void> {
     if (apply) {
       return myCopy(this.defaultDockerFilePath, this.dockerFilePath).then(
