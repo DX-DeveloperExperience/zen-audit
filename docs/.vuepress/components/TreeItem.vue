a7ac6ffdbdff6e19941960aa29bd37bafee0eed7
ig1na <greenig1na@gmail.com>
2019-08-23 16:28:45 +0200

    docs(tree-item): Add tree-item vue component to list classes

---
+++ docs/.vuepress/components/TreeItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    <div :class="{bold: isFolder}" @click="toggle">
+      {{ item.name }}
+      <span v-if="isFolder">[{{ isOpen ? '-' : '+' }}]</span>
+    </div>
+    <ul v-show="isOpen" v-if="isFolder">
+      <tree-item
+        class="item"
+        v-for="(child, index) in item.children"
+        :key="index"
+        :item="child"
+        @make-folder="$emit('make-folder', $event)"
+        @add-item="$emit('add-item', $event)"
+      ></tree-item>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  data: function() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    isFolder: function() {
+      return this.item.children && this.item.children.length;
+    },
+  },
+  methods: {
+    toggle: function() {
+      if (this.isFolder) {
+        this.isOpen = !this.isOpen;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.item {
+  cursor: pointer;
+}
+</style>
